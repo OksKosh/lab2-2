fe76382e064023b821e2df76e0a82e3c946645a0
Oks Kosh <oksana.pavletsova.00@bk.ru>
2020-10-27 13:24:00 +0300

    feat: menu added

--- reader.cpp
+++ reader.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
-#include <windows.h>
+#include <limits>
+
+int get_option() {
+	std::cout << "\nEnter option:\n";
+	std::cout << "1 - Open mapping file\n";
+	std::cout << "2 - Map file\n";
+	std::cout << "3 - Read data\n";
+	std::cout << "0 - Exit\n";
+	
+	int option = 0;
+	std::cin >> option;
+	
+	while (std::cin.fail() || option > 3 || option < 0) {
+		std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        
+		std::cout << "Incorrect option. Please, try again\n";		
+		std::cin >> option;
+	}
+	
+	return option;
+}
+
+void open_file() {
+	
+}
+
+void map_file() {
+	
+}
+
+void read_data() {
+	
+}
+
+void (*function_pointers[4])() = {
+	nullptr,
+	open_file,
+	map_file,
+	read_data,
+};
 
 int main() {
+	int option = 0;
+	
+	while ((option = get_option()) != 0) {
+		(*function_pointers[option])();
+	}
 	
 	return 0;
 }
--- writer.cpp
+++ writer.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
-#include <windows.h>
+#include <limits>
+
+int get_option() {
+	std::cout << "\nEnter option:\n";
+	std::cout << "1 - Create mapping file\n";
+	std::cout << "2 - Map file\n";
+	std::cout << "3 - Write data\n";
+	std::cout << "0 - Exit\n";
+	
+	int option = 0;
+	std::cin >> option;
+	
+	while (std::cin.fail() || option > 3 || option < 0) {
+		std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        
+		std::cout << "Incorrect option. Please, try again\n";		
+		std::cin >> option;
+	}
+	
+	return option;
+}
+
+void create_file() {
+	
+}
+
+void map_file() {
+	
+}
+
+void write_data() {
+	
+}
+
+void (*function_pointers[4])() = {
+	nullptr,
+	create_file,
+	map_file,
+	write_data,
+};
 
 int main() {
+	int option = 0;
+	
+	while ((option = get_option()) != 0) {
+		(*function_pointers[option])();
+	}
 	
 	return 0;
 }
